2a3684d14e3a79669b68c3d7905dbb12e30582ed
mtraworld <kylewong@traworld.com>
2025-04-11 11:07:43 +0800

    feat(setup): add user command with `DotMd` prefix during setup

--- lua/dotmd/config.lua
+++ lua/dotmd/config.lua
@@ -76,6 +76,69 @@ local defaults = {
 ---@param user_config? DotMd.Config
 function M.setup(user_config)
 	M.config = vim.tbl_deep_extend("force", defaults, user_config or {})
+
+	M.setup_user_commands()
+end
+
+function M.setup_user_commands()
+	local commands = require("dotmd.commands")
+	local utils = require("dotmd.utils")
+
+	vim.api.nvim_create_user_command("DotMdCreateNote", function(opts)
+		local args = opts.args
+		opts = utils.prepare_user_command_args(args)
+		commands.create_note(opts)
+	end, {
+		nargs = "?",
+		desc = "Create a new note",
+	})
+
+	vim.api.nvim_create_user_command("DotMdCreateTodoToday", function(opts)
+		local args = opts.args
+		opts = utils.prepare_user_command_args(args)
+		commands.create_todo_today(opts)
+	end, {
+		nargs = "?",
+		desc = "Create a new todo for today",
+	})
+
+	vim.api.nvim_create_user_command("DotMdCreateJournal", function(opts)
+		local args = opts.args
+		opts = utils.prepare_user_command_args(args)
+		commands.create_journal(opts)
+	end, {
+		nargs = "?",
+		desc = "Create a new journal entry",
+	})
+
+	vim.api.nvim_create_user_command("DotMdInbox", function(opts)
+		local args = opts.args
+		opts = utils.prepare_user_command_args(args)
+		commands.inbox(opts)
+	end, {
+		nargs = "?",
+		desc = "Create or open the inbox",
+	})
+
+	vim.api.nvim_create_user_command("DotMdPick", function(opts)
+		local args = opts.args
+		opts = utils.prepare_user_command_args(args)
+		commands.pick(opts)
+	end, {
+		nargs = "?",
+		desc = "Pick a file from a list of directories",
+	})
+
+	vim.api.nvim_create_user_command("DotMdNavigate", function(opts)
+		local direction = opts.args or "next"
+		commands.navigate(direction)
+	end, {
+		nargs = "?",
+		complete = function()
+			return { "previous", "next" }
+		end, -- provide argument completions
+		desc = "Navigate to the nearest previous/next date-based file",
+	})
 end
 
 return M
